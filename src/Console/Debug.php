456848fd1d452d70d7cd8ce621ce6a56d9c3c0e7
Xavier Barbosa <xavier@madewithlove.be>
2018-03-29 21:12:53 +0100

    Better handling when there are no storage files, code clean up.

--- src/Console/Debug.php
+++ src/Console/Debug.php
@@ -20,7 +20,7 @@ class Debug extends Command
      *
      * @var string
      */
-    protected $description = 'Collects query data and stats.';
+    protected $description = 'Displays laravel debug bar stored information.';
 
     /**
      * Execute the console command.
@@ -37,35 +37,35 @@ class Debug extends Command
             try {
                 $latestFile = $debug->getStorage()->find([], 1);
             } catch (InvalidArgumentException $exception) {
+                $this->refresh();
                 $this->error($exception->getMessage());
 
-                return;
+                continue;
             }
 
             $id = array_get($latestFile, '0.id');
 
             // No file found so continue
             if (empty($id)) {
-                sleep(1);
+                $this->refresh();
+                $this->warn('No laravel debugbar stored files available. Do a request and make sure APP_DEBUG is set to true.');
 
                 continue;
             }
 
             // Latest file is the same do not render
             if (array_get($profileData, '__meta.id') === $id) {
-                sleep(1);
+                $this->wait();
 
                 continue;
             }
 
             $profileData = $debug->getStorage()->get($id);
 
-            system('clear');
+            $this->refresh();
             $this->info('Latest Request Information:');
             $this->renderMetaData($profileData);
             $this->renderQueryData($profileData);
-
-            sleep(1);
         }
     }
 
@@ -85,6 +85,17 @@ class Debug extends Command
                 array_get($data, 'time.duration_str')
             ]
         ]);
+    }
+
+    public function wait()
+    {
+        sleep(1);
+    }
+
+    public function refresh()
+    {
+        $this->wait();
+        system('clear');
     }
 
     /**
